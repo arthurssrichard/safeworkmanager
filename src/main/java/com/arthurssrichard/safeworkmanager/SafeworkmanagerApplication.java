cade01d615227b45d1615aa6aa039993569a6802
arthurssrichard <arthurssrichard@gmail.com>
2024-11-06 19:42:17 -0300

    Initial Commit

---
+++ src/main/java/com/arthurssrichard/safeworkmanager/SafeworkmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.arthurssrichard.safeworkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeworkmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeworkmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurssrichard/safeworkmanager/SafeworkmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurssrichard.safeworkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeworkmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
