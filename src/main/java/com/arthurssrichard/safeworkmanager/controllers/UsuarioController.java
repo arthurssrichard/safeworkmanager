1661db2cf0dd80ba91162f07d1e478a115e424df
arthurssrichard <arthurssrichard@gmail.com>
2024-11-16 18:28:45 -0300

    usuarios - show criado
    pagina index de usuarios listados do sistema feita

--- src/main/java/com/arthurssrichard/safeworkmanager/controllers/UsuarioController.java
+++ src/main/java/com/arthurssrichard/safeworkmanager/controllers/UsuarioController.java
@@ -1,15 +1,43 @@
 package com.arthurssrichard.safeworkmanager.controllers;
 
+import com.arthurssrichard.safeworkmanager.config.UsuarioService;
+import com.arthurssrichard.safeworkmanager.models.Usuario;
+import com.arthurssrichard.safeworkmanager.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class UsuarioController {
+    @Autowired
+    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioRepository usuarioRepository, UsuarioService usuarioService) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping("/login")
     public ModelAndView login(){
         ModelAndView mv = new ModelAndView("login");
+        return mv;
+    }
+    @GetMapping("/usuarios")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("usuarios/index");
+        Usuario usuario = usuarioService.getLoggedUser();
+        List<Usuario> usuarios = usuarioRepository.findByEmpresa(usuario.getEmpresa());
+        mv.addObject("usuarios", usuarios);
+        return mv;
+    }
+    @GetMapping("/usuarios/new")
+    public ModelAndView nnew(){
+        ModelAndView mv = new ModelAndView("usuarios/new");
         return mv;
     }
 }
--- src/main/java/com/arthurssrichard/safeworkmanager/repositories/UsuarioRepository.java
+++ src/main/java/com/arthurssrichard/safeworkmanager/repositories/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.arthurssrichard.safeworkmanager.repositories;
 
+import com.arthurssrichard.safeworkmanager.models.Empresa;
 import com.arthurssrichard.safeworkmanager.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Usuario findByNome(String nome);
+    List<Usuario> findByEmpresa(Empresa empresa);
 }
