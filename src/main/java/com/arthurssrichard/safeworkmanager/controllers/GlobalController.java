58052051c9cf695dcee3e7da4fd6e49f3034249a
arthurssrichard <141867186+arthurssrichard@users.noreply.github.com>
2024-11-23 23:08:49 -0300

    fix: impossibilitar usuário de apagar ele mesmo

--- src/main/java/com/arthurssrichard/safeworkmanager/controllers/GlobalController.java
+++ src/main/java/com/arthurssrichard/safeworkmanager/controllers/GlobalController.java
@@ -1,13 +1,26 @@
 package com.arthurssrichard.safeworkmanager.controllers;
 
+import com.arthurssrichard.safeworkmanager.config.UsuarioService;
+import com.arthurssrichard.safeworkmanager.models.Usuario;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class GlobalController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
     @ModelAttribute("servletPath")
     public String getRequestServletPath(HttpServletRequest request){
         return request.getServletPath();
+    }
+
+    @ModelAttribute("loggedUser")
+    public Usuario getLoggedUser(Usuario usuario){
+        usuario = usuarioService.getLoggedUser();
+        return usuario;
     }
 }
